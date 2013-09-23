28abd145ce3a55e5743d8c051bb4f375a907cdb0
Attsun1031 <atsumitatsuya@progress.local>
2013-09-23 13:53:24 +0900

    リスト登録系ロジック実装

--- app/models/check_list.rb
+++ app/models/check_list.rb
@@ -1,4 +1,19 @@
+# coding: utf-8
+
+# チェックリスト
 class CheckList < ActiveRecord::Base
   belongs_to :user
   attr_accessible :name
+
+  def add_item(product, comment = "")
+    item = ListItem.new
+    item.register(self, product, comment)
+  end
+
+  def self.register(user, name)
+    new_list = self.new
+    new_list.name = name
+    new_list.user_id = user.id
+    new_list.save
+  end
 end
--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,5 +1,22 @@
+# coding: utf-8
+
+# チェックリストに存在する１アイテム
 class ListItem < ActiveRecord::Base
   belongs_to :check_list
   belongs_to :product
   attr_accessible :checked, :comment, :image_path
+
+  def self.register(check_list, product, comment)
+    # 未登録の product なら保存
+    unless Product.exists?(:asin => product.asin)
+      product.save
+    end
+
+    list = self.new
+    list.check_list_id = check_list.id
+    list.product_id = product.id
+    list.comment = comment
+    list.image_path = product.image_url
+    list.save
+  end
 end
